8b26bb5aebc09378b985b11be4c18370e0d89673
Noé Rivera Zevallos <85976759+myron98980@users.noreply.github.com>
2025-09-04 19:03:24 -0500

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 
 // Crea el pool de conexiones a la base de datos
 const pool = mysql.createPool({
-  host: 'localhost',       // O la IP de tu servidor de base de datos
+  host: 'srv1508.hstgr.io',       // O la IP de tu servidor de base de datos
   user: 'u784147396_noe56',            // Tu usuario de MySQL
   password: 'n&QoAAP33', // <-- CAMBIA ESTO por tu contraseña de MySQL
   database: 'u784147396_banco_warmi', // El nombre de tu base de datos
